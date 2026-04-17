7dc01d4ff093b30984d131ad3210c92e58fb937f
Krish-Arun <krish.official.v1@gmail.com>
2026-04-17 07:19:15 +0000

    phase2: populate tree entries from index

--- tree.c
+++ tree.c
@@ -129,11 +129,23 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 //   - object_write    : save that binary buffer to the store as OBJ_TREE
 //
 // Returns 0 on success, -1 on error.
+
 int tree_from_index(ObjectID *id_out) {
     Index index;
 
     if (index_load(&index) != 0) {
         return -1;
+    }
+
+    Tree tree;
+    tree.count = 0;
+
+    for (int i = 0; i < index.count; i++) {
+        tree.entries[tree.count].mode = index.entries[i].mode;
+        tree.entries[tree.count].hash = index.entries[i].hash;
+
+        strcpy(tree.entries[tree.count].name, index.entries[i].path);
+        tree.count++;
     }
 
     (void)id_out;
